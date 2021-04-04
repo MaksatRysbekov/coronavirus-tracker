a839e5019aa3db28917bbc8e4a6f467b79108095
MaksatRysbekov <maksatrysbekov96@gmail.com>
2021-04-04 13:52:44 +0200

    add model class

---
+++ src/main/java/com/maxdev/coronatrackingapp/models/LocationStats.java
@@ -0,0 +1,15 @@
+package com.maxdev.coronatrackingapp.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Builder
+@Data
+public class LocationStats {
+    private String state;
+    private String country;
+    private int latestReportedCases;
+}
--- src/main/java/com/maxdev/coronatrackingapp/services/CoronavirusDataService.java
+++ src/main/java/com/maxdev/coronatrackingapp/services/CoronavirusDataService.java
@@ -1,5 +1,8 @@
 package com.maxdev.coronatrackingapp.services;
 
+import com.maxdev.coronatrackingapp.models.LocationStats;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,27 +12,36 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@Slf4j
 public class CoronavirusDataService {
     private final static String CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private List<LocationStats> allStats = new ArrayList<>();
 
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchCoronaData() throws IOException {
+        List<LocationStats> newStats = new ArrayList<>();
         RestTemplate restTemplate = new RestTemplate();
         String response = restTemplate.getForObject(CORONA_DATA_URL, String.class);
-        System.out.println(response);
-
 
         assert response != null;
         StringReader csvReader = new StringReader(response);
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvReader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
-
+            LocationStats stats = LocationStats.builder()
+                                        .state(record.get("Province/State"))
+                                        .country(record.get("Country/Region"))
+                                        .latestReportedCases(Integer.parseInt(record.get(record.size() - 1)))
+                                        .build();
+            newStats.add(stats);
+            log.info("Received data: {}", stats);
         }
+
+        allStats = newStats;
 
     }
 }
