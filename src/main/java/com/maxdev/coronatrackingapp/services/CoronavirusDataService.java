b93a699ec71be624590656ca73c1a77ba173c464
MaksatRysbekov <maksatrysbekov96@gmail.com>
2021-04-04 13:29:07 +0200

    add csv reader and scheduling

--- src/main/java/com/maxdev/coronatrackingapp/services/CoronavirusDataService.java
+++ src/main/java/com/maxdev/coronatrackingapp/services/CoronavirusDataService.java
@@ -1,12 +1,16 @@
 package com.maxdev.coronatrackingapp.services;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.io.*;
 import java.time.Duration;
 
 @Service
@@ -14,9 +18,21 @@ public class CoronavirusDataService {
     private final static String CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
-    public void fetchCoronaData() {
+    @Scheduled(cron = "* * 1 * * *")
+    public void fetchCoronaData() throws IOException {
         RestTemplate restTemplate = new RestTemplate();
         String response = restTemplate.getForObject(CORONA_DATA_URL, String.class);
         System.out.println(response);
+
+
+        assert response != null;
+        StringReader csvReader = new StringReader(response);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+
+        }
+
     }
 }
