513ed13fe859390661f4dc2a68153208f1b9bffc
MaksatRysbekov <maksatrysbekov96@gmail.com>
2021-04-04 12:06:25 +0200

    getting data in console

---
+++ src/main/java/com/maxdev/coronatrackingapp/services/CoronavirusDataService.java
@@ -0,0 +1,22 @@
+package com.maxdev.coronatrackingapp.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+
+@Service
+public class CoronavirusDataService {
+    private final static String CORONA_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    @PostConstruct
+    public void fetchCoronaData() {
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(CORONA_DATA_URL, String.class);
+        System.out.println(response);
+    }
+}
---
+++ src/test/java/com/maxdev/coronatrackingapp/CoronaTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxdev.coronatrackingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
